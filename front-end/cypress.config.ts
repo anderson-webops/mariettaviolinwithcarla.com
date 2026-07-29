1d8433c58c537ae82802bb6b029966d2a91da5dd
Jacob Anderson <jacobdanderson@gmail.com>
2026-07-29 03:21:22 -0400

    Restore audited Cypress coverage

--- front-end/cypress.config.ts
+++ front-end/cypress.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from "cypress";
 const baseUrl = process.env.CYPRESS_BASE_URL || "http://localhost:3333";
 
 export default defineConfig({
+	allowCypressEnv: false,
+	blockHosts: ["analytics.jacobdanderson.net", "analytics.mariettaviolinwithcarla.com"],
 	e2e: {
 		baseUrl,
 		setupNodeEvents() {
