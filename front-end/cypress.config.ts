86ec3e7b3bad763efc73f832729a5d4de0a67e85
Jacob Anderson <jacobdanderson@gmail.com>
2026-09-21 21:57:25 -0400

    Release v1.5.1 dependency and CI refresh

--- front-end/cypress.config.ts
+++ front-end/cypress.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "cypress";
 const baseUrl = process.env.CYPRESS_BASE_URL || "http://localhost:3333";
 
 export default defineConfig({
-	allowCypressEnv: false,
 	blockHosts: ["analytics.mariettaviolinwithcarla.com"],
 	e2e: {
 		baseUrl,
